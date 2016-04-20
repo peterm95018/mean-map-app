da664fd706f4f9eb73b0e37214a34c0ef9e4cd11
Peter McMillan <peterm95018@gmail.com>
2016-04-20 14:48:46 -0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meanMapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the meanMapApp
+ */
+angular.module('meanMapApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
