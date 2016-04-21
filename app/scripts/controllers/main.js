5f8b7e6f99de17e65ce57ddcabeb849b9d093b68
Peter McMillan <peterm@ucsc.edu>
2016-04-20 19:09:48 -0700

    updated controller with leaflet extend scope example

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,33 @@
  * # MainCtrl
  * Controller of the meanMapApp
  */
+// angular.module('meanMapApp')
+//   .controller('MainCtrl', [ '$scope', function ($scope) {
+//     // this.awesomeThings = [
+//     //   'HTML5 Boilerplate',
+//     //   'AngularJS',
+//     //   'Karma'
+//     // ];
+
+   
+//     angular.extend($scope, {
+//         defaults: {
+//             scrollWheelZoom: false
+//         }
+//     });
+
+//   }]);
+
 angular.module('meanMapApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller("MainCtrl", [ '$scope', function($scope) {
+    angular.extend($scope, {
+        center: {
+            lat: 40.095,
+            lng: -3.823,
+            zoom: 4
+        },
+        defaults: {
+            scrollWheelZoom: false
+        }
+    });
+}]);
