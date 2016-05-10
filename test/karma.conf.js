62b054cbffac7b3a76c0b7339d7bf18765c1490b
Peter McMillan <peterm95018@gmail.com>
2016-05-10 15:47:20 -0700

    readded files and updates from various tweaks

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -37,6 +37,7 @@ module.exports = function(config) {
       'bower_components/leaflet/dist/leaflet-src.js',
       'bower_components/ui-leaflet/dist/ui-leaflet.js',
       'bower_components/leaflet.markercluster/dist/leaflet.markercluster-src.js',
+      'bower_components/Leaflet.awesome-markers/dist/leaflet.awesome-markers.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
